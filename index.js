9b60c1468e2b1e48cc094d70e87927dc58839072
Steven Smethurst <funvill@funvill.com>
2023-05-01 18:59:40 -0700

    Answered a bunch of questions

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // 
 // Load a list of elements from a file, one element per line, ask the user to compare two elements, save the results to a file
 // if the user has already answered the question, skip it. sort the list of elements based on the answers. Best answers first.
+// The elements file is updated after each question is asked with the new sorting order.
 // 
 // I am using this to rank the ideas from the https://blog.abluestar.com/projects/2023-100-ideas/
 
@@ -21,14 +22,19 @@ var questionCount = 1;
 // Format is: "1 vs 2 == 1" or "1 vs 2 == 2" 
 function SaveAnswer(answer, element1, element2) {
   fs.appendFileSync(FILE_ANSWERS, element1.index + " vs " + element2.index + " == " + (answer == 'a' ? element1.index : element2.index) + "\n");
-  
+
 }
 
 // Search the answers file to see if we have already asked this question
 // Format is: "1 vs 2 == 1" or "1 vs 2 == 2"
 // Being lazy here, reload the file each time, and search for the 4 different combinations
 function SearchForAnswer(element1, element2) {
-  var answers = fs.readFileSync(FILE_ANSWERS).toString();
+  try {
+    var answers = fs.readFileSync(FILE_ANSWERS).toString();
+  } catch (e) {
+    // If the file doesn't exist then we have not asked this question yet
+    return null;
+  }
 
   var a = element1.index + " vs " + element2.index + " == " + element1.index
   var b = element1.index + " vs " + element2.index + " == " + element2.index
@@ -48,13 +54,13 @@ function SearchForAnswer(element1, element2) {
 // The user must enter in "a" or "b" to indicate which element is better
 // Return true if element1 is better than element2
 function Question(element1, element2) {
-  var question = questionCount + ") Which is better? \na) (#" + element1.index + ") " + element1.value + "\nb) (#" + element2.index + ") " + element2.value + "\n";
+  var question = "\n" + questionCount + ") Which is better? \na) (#" + element1.index + ") " + element1.value + "\nb) (#" + element2.index + ") " + element2.value;
   console.log(question);
 
   // Search the answers file to see if we have already asked this question
   var results = SearchForAnswer(element1, element2);
   if (results != null) {
-    console.log("FYI: We have already asked this question. " + (results ? element1.index : element2.index) + " > " + (results ? element2.index : element1.index));
+    console.log("FYI: We have already asked this question. " + (results ? element1.index : element2.index) + " > " + (results ? element2.index : element1.index) + "\n");
     return results;
   }
 
@@ -73,38 +79,97 @@ function Question(element1, element2) {
   }
 }
 
-// QuickSort is one of the possiable algorithms to sort the list of elements
-// https://en.wikipedia.org/wiki/Quicksort
-function QuickSort(array) {
-  if (array.length <= 1) {
-    return array;
+function swap(array, i, j) {
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+
+  WriteSortedElementsToFile(array);
+}
+
+// Bubble sort the array, worse case O(n^2), best case O(n)
+// This is the easist to implement, but not the most efficient, used it to 
+// get the project started
+function BubbleSort(array) {
+  var swapped;
+  do {
+    swapped = false;
+    for (var i = 0; i < array.length - 1; i++) {
+      if (!Question(array[i], array[i + 1])) {
+        swap(array, i, i + 1);
+        swapped = true;
+        console.log("\n");
+        questionCount++;
+      }
+    }
+  } while (swapped);
+  return array;
+}
+
+// HeapSort of the array using swap to change values in the array.
+// No memory is allocated, the array is sorted in place.
+// Worse case O(n log n)
+// Average case O(n log n)
+function HeapSort(array) {
+
+  // Build the heap in array so that largest value is at the root
+  for (var i = Math.floor(array.length / 2) - 1; i >= 0; i--) {
+    Heapify(array, array.length, i);
   }
 
-  var pivot = array[0];
+  // Move the root to the end of the array
+  for (var i = array.length - 1; i >= 0; i--) {
+    swap(array, 0, i);
 
-  var left = [];
-  var right = [];
+    // Heapify the reduced heap
+    Heapify(array, i, 0);
+  }
+  return array;
+}
 
-  for (var i = 1; i < array.length; i++) {
-    Question(array[i], pivot) ? left.push(array[i]) : right.push(array[i]);
-    console.log("\n");
+// Heapify the array
+// n is the size of the heap
+// i is the index of the root
+function Heapify(array, n, i) {
+  var largest = i; // Initialize largest as root
+  var l = 2 * i + 1; // left = 2*i + 1
+  var r = 2 * i + 2; // right = 2*i + 2
+
+  // If left child is larger than root
+  if (l < n && Question(array[largest], array[l])) {
+    largest = l;
     questionCount++;
   }
 
-  return QuickSort(left).concat(pivot, QuickSort(right));
-};
+  // If right child is larger than largest so far
+  if (r < n && Question(array[largest], array[r])) {
+    largest = r;
+    questionCount++;
+  }
+
+  // If largest is not root
+  if (largest != i) {
+    swap(array, i, largest);
+
+    // Recursively heapify the affected sub-tree
+    Heapify(array, n, largest);
+  }
+}
 
 // This is the sorting algorithm to sort the list of elements
 // returns the sorted array.
 function SortQuestions(array) {
-  return QuickSort(array);
+  // return HeapSort(array);
+  return BubbleSort(array);
 }
 
 function WriteSortedElementsToFile(sorted) {
-  var file = fs.createWriteStream(FILE_ANSWERS);
-  file.on('error', function (err) { /* error handling */ });
-  sorted.forEach(function (v) { file.write(v.index + ") - " + v.value + "\n"); });
-  file.end();
+
+  var fileoutput = "";
+  for (var i = 0; i < sorted.length; i++) {
+    fileoutput += sorted[i].index + ") - " + sorted[i].value + "\n";
+  }
+  fs.writeFileSync(FILE_ELEMENTS, fileoutput);
 }
 
 function LoadElmentsFromFile(filename) {
@@ -116,20 +181,17 @@ function LoadElmentsFromFile(filename) {
     if (unsorted[i] == "") {
       unsorted.splice(i, 1);
     }
-
-    // Remove any trailing spaces
-    unsorted[i] = unsorted[i].trim();
   }
 
   // The string will have a "{index}) - {value}" format
   // Extract the index and value
   for (var i = 0; i < unsorted.length; i++) {
     var index = unsorted[i].split(") - ")[0].replace("(", "");
-    var value = unsorted[i].split(") - ")[1].replace(" - ", "").trim();
+    var value = unsorted[i].split(") - ")[1].trim();
 
     // Strip any non alpha numeric characters
     value = value.replace(/[^a-zA-Z0-9 \-]/g, "");
-  
+
     unsorted[i] = { index: index, value: value };
   }
   return unsorted;
@@ -145,12 +207,16 @@ console.log("Unsorted: ", unsorted);
 
 // Tell the users the max amount of questions they will be asked based on QuickSort algorithm 
 // Average case: O(n log n)
-// Worst case: O(n^2)
+// Worst case: O(n log n)
 const elment_count = unsorted.length;
 var average_number_of_questions = Math.round(elment_count * Math.log(elment_count));
-var worst_number_of_questions = Math.round(elment_count * Math.log2(elment_count));
+var worst_number_of_questions = Math.round(elment_count * Math.log(elment_count));
 
-console.log("You will be asked " + worst_number_of_questions + " questions at most, average of: " + average_number_of_questions + "\n");
+console.log("You will be asked " + worst_number_of_questions + " questions at most, average of: " + average_number_of_questions);
+
+// Count the number of lines in the answers file
+var answer_count = fs.readFileSync(FILE_ANSWERS).toString().split("\n").length;
+console.log("You have already answered " + answer_count + " questions");
 
 var sorted = SortQuestions(unsorted);
 console.log('Sorted: ', sorted);
